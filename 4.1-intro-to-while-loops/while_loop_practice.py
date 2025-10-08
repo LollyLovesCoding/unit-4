3e14dd63c8c44611c080e699291687074d4e0116
Lilyana Zhang <impro4everrr@gmail.com>
2025-10-08 10:24:08 -0400

    Create while_loop_practice.py

---
+++ 4.1-intro-to-while-loops/while_loop_practice.py
@@ -0,0 +1,59 @@
+# 1
+n = 0
+while n <= 10:
+    print(n)
+    n += 1
+
+# 2
+n = 10
+while n >= 0:
+    print(n)
+    n -= 1
+
+# 3
+n = 491
+while n <= 586:
+    print(n)
+    n += 5
+
+# 4
+n = 124
+while n <= 810:
+    print(n)
+    n += 14
+
+# 5
+n = -233
+while n <= -113:
+    print(n)
+    n += 4
+
+# 6
+n = 215
+while n <= 300:
+    print(n)
+    n += 5
+
+# 7
+n = -310
+while n <= -94:
+    print(n)
+    n += 8
+
+# 8
+n = -476
+while n <= -410:
+    print(n)
+    n += 3
+
+# 9
+n = -327
+while n <= -147:
+    print(n)
+    n += 9
+
+# 10
+n = 484
+while n <= 1024:
+    print(n)
+    n += 18
