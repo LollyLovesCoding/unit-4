e6eeffc6b952442950f9e4d9da8786bd2275db06
Lilyana Zhang <impro4everrr@gmail.com>
2025-10-30 10:25:32 -0400

    Create for_loop_challenge.py

---
+++ 4.6-for-loops/for_loop_challenge.py
@@ -0,0 +1,13 @@
+# Write a for loop that prints out all the numbers from {start} to {end} counting by {increment}
+start = 0
+end = 10
+increment = 1
+for n in range(start, end + 1, increment):
+    print(n)
+
+# “Write a for loop that prints out all the numbers from 15 to 30, counting by 3s”
+start = 15
+end = 30
+increment = 3
+for n in range(start, end + 1, increment):
+    print(n)
